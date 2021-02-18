7a74872ee54da1d1a666c4ce0fa74200e9ba30ec
Alexandre Stein <astein58@gmail.com>
2021-02-18 19:19:59 +0100

    Very small improvement on the build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cargo build --release --target x86_64-unknown-linux-musl
+cargo build --release --bins --target x86_64-unknown-linux-musl
 
-cargo build --release --target x86_64-pc-windows-gnu
+cargo build --release --bins --target x86_64-pc-windows-gnu
