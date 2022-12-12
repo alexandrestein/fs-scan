0179bc6dc872ea45be8f469762e6383f0b402a09
Alexandre Stein <stein_alexandre@pm.me>
2022-12-12 12:14:10 +0100

    Make the build script more effective and building some kind of package

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+ORIGIN=$(pwd)
+BIN_DEST="/tmp/fs-scan-bin"
+
 cargo build --release --bins --target x86_64-unknown-linux-musl
 
 cargo build --release --bins --target x86_64-pc-windows-gnu
+
+mkdir -p "${BIN_DEST}"
+cp target/x86_64-unknown-linux-musl/release/fs-scan "${BIN_DEST}/fs-scan.bin"
+cp target/x86_64-pc-windows-gnu/release/fs-scan.exe "${BIN_DEST}/"
+
+cd ${BIN_DEST}
+
+md5sum fs-scan.* > "signature-fs-scan.md5"
+b3sum fs-scan.* > "signature-fs-scan.b3sum"
+sha256sum fs-scan.* > "signature-fs-scan.sha256"
+sha1sum fs-scan.* > "signature-fs-scan.sha1"
+
+tar -cvzf "${ORIGIN}/fs-scan-binary.tgz" *
+
+rm -rf "${BIN_DEST}"
